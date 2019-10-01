b0ea85a33e8423ba363b06976a1260d77d7b5849
Tamás Viczián <viczian.t@gmail.com>
2019-10-01 15:04:09 +0200

    user model get, post request

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,28 @@ const models = require('../models');
 router.get('', function(req, res, next) {
   return models.User.findAll().then(users => {
     res.json({ users })
-  })
+  });
+});
+
+// TODO add error handling. Globally...
+router.get('/:id', function(req, res, next) {
+  return models.User.findByPk(req.params.id).then(users => {
+    if (!users) res.sendStatus(404);
+    res.json({ users })
+  });
+});
+
+router.post('', function(req, res, next) {
+  return models.User.create({
+    username: req.body.username,
+    email: req.body.email,
+    bio: req.body.bio,
+    image: req.body.image,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }).then(() => {
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
