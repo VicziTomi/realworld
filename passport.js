a3dcb6f74f06c48e93ea8db0f9fca8ab37aca34f
Tamás Viczián <viczian.t@gmail.com>
2019-11-07 09:04:34 +0100

    authentication working

--- passport.js
+++ passport.js
@@ -11,7 +11,12 @@ passport.use(new LocalStrategy({
     passwordField: 'password'
   }, 
   async (email, password, cb) => {
-    return await models.User.findOne({email, password}) //TODO finish db query!
+    return await models.User.findOne({
+      where: {
+        email: email,
+        password: password
+      }
+    })
     .then(user => {
       if (!user) {
         return cb(null, false, {message: 'Incorrect email or password.'});
@@ -24,10 +29,9 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey   : 'your_jwt_secret'
+    secretOrKey   : 'softKitty'
   },
   function (jwtPayload, cb) {
-    //find the user in db if needed
     return models.User.findOneById(jwtPayload.id)
     .then(user => {
       return cb(null, user);
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 router.post('/login', function (req, res, next) {
   passport.authenticate('local', {session: false}, (err, user, info) => {
     if (err || !user) {
+      console.log(err);
       return res.status(400).json({
         message: info ? info.message : 'Login failed',
         user: user
@@ -15,8 +16,7 @@ router.post('/login', function (req, res, next) {
       if (err) {
         res.send(err);
       }
-      // generate a signed son web token with the contents of user object and return it in the response
-      const token = jwt.sign(user, 'your_jwt_secret');
+      const token = jwt.sign(user.toJSON(), 'softKitty');
       return res.json({user, token});
     });
   })
